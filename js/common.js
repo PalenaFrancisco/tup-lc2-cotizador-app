faa81748531dd3e3f9a78391f6430b406bc9d566
MarucoMass <mariomass23@gmail.com>
2024-05-29 15:07:46 -0300

    fetch en index.html para mostrar las monedas

--- js/common.js
+++ js/common.js
@@ -1,1 +1,24 @@
-/*Codigo que afectan a más de una pantalla*/
+const padre = document.querySelector("#ver-moneda");
+
+fetch("https://dolarapi.com/v1/dolares")
+  .then((response) => response.json())
+  .then((monedas) => {
+    monedas.forEach((moneda) => {
+      padre.innerHTML += `<div class="contenido_main--box--card">
+      <h3>${moneda.nombre}</h3>
+      <div class="contenido_main--box--precios">
+        <div class="compra">
+          <h4>COMPRA</h4>
+          <p>$${moneda.compra}</p>
+        </div>
+        <div class="venta">
+          <h4>VENTA</h4>
+          <p>$${moneda.venta}</p>
+        </div>
+        <input type="checkbox" id="starCheckbox-${moneda.nombre}" class="starCheckbox">
+        <label for="starCheckbox-${moneda.nombre}" class="starCheckboxLabel"></label>
+      </div>
+    </div>`;
+    });
+  })
+  .catch((error) => console.log(error));
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+    //   padre.innerHTML += `<div class="contenido_main--box--card">
+    //   <h3>${item.nombre}</h3>
+    //   <div class="contenido_main--box--precios">
+    //     <div class="compra">
+    //       <h4>COMPRA</h4>
+    //       <p>$${item.compra}</p>
+    //     </div>
+    //     <div class="venta">
+    //       <h4>VENTA</h4>
+    //       <p>$${item.venta}</p>
+    //     </div>
+    //     <input type="checkbox" id="starCheckbox1" class="starCheckbox">
+    //     <label for="starCheckbox1" class="starCheckboxLabel"></label>
+    //   </div>
+    // </div>`;
