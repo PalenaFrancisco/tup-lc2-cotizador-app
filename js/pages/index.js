5cce8f11f2c26056bc8df5e3033a36a93d6bf780
MarucoMass <mariomass23@gmail.com>
2024-06-12 15:30:00 -0300

    index.js: seteamos el localStorage

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,8 +1,10 @@
 const padre = document.querySelector("#ver-moneda");
 const selectorMoneda = document.getElementById("moneda");
-const API = "https://dolarapi.com/v1/dolares";
+const favoritosBtns = document.querySelectorAll(".starCheckbox");
+let listaFavoritos = [];
+const API = "https://dolarapi.com/v1/";
 
-async function llamadaAPI(param = "") {
+async function llamadaAPI(param = "dolares") {
   try {
     const response = await fetch(`${API}${param}`);
     const data = await response.json();
@@ -15,9 +17,9 @@ async function llamadaAPI(param = "") {
 
 async function renderMonedas(param) {
   const monedas = await llamadaAPI(param);
-  padre.innerHTML = ""
-  if(monedas.length > 1) {
-    for(let i = 0; i < monedas.length; i++) {
+  padre.innerHTML = "";
+  if (monedas.length > 1) {
+    for (let i = 0; i < monedas.length; i++) {
       armarHTML(monedas[i]);
     }
   } else {
@@ -37,7 +39,7 @@ function armarHTML(moneda) {
       <h4>VENTA</h4>
       <p>$${moneda.venta}</p>
     </div>
-    <input type="checkbox" id="starCheckbox-${moneda.nombre}" class="starCheckbox">
+    <input type="checkbox" id="starCheckbox-${moneda.nombre}" data-attribute=${moneda.casa} onclick="agregarAlStorage(this)" class="starCheckbox">
     <label for="starCheckbox-${moneda.nombre}" class="starCheckboxLabel"></label>
   </div>
 </div>`;
@@ -48,4 +50,23 @@ selectorMoneda.addEventListener("change", (event) => {
   renderMonedas(data);
 });
 
+async function agregarAlStorage(e) {
+  const key = e.dataset.attribute;
+  const data = await llamadaAPI(`dolares/${key}`);
+
+  const favorito = {
+    nombre: data.nombre,
+    compra: data.compra,
+    venta: data.venta,
+    fechaActualizacion: data.fechaActualizacion,
+  };
+  setearStorage(favorito);
+}
+
+function setearStorage(favorito) {
+  listaFavoritos.push(favorito);
+  localStorage.setItem("favoritos", JSON.stringify(listaFavoritos));
+}
+
 renderMonedas();
+
